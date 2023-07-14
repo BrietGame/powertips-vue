77d1c3e48857d1b8f0f957b60b7b4afd0e826c34
Alexis BRIET <alexis.briet2003@gmail.com>
2023-07-14 14:29:09 +0200

    Fix button on home to createGuide

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
       <div class="mr-auto place-self-center lg:col-span-7">
         <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Partagez vos connaissances</h1>
         <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Vous savez faire une recette de cuisine et que vous souhaitez la partager pour en faire profiter tout le monde ? Alors <span class="text-bold">inscrivez-vous</span> dès maintenant et partagez vos connaissances.</p>
-        <RouterLink to="/register" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+        <RouterLink :to="isConnect ? '/create-guide' : '/register'" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
           Commencer
           <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </RouterLink>
@@ -28,6 +28,9 @@ export default {
   name: 'Home',
   components: {GuideCard, Loading},
   computed: {
+    isConnect() {
+      return this.$store.getters.getIsConnected;
+    },
     guides() {
       return this.$store.getters.getGuidesPublished;
     }
